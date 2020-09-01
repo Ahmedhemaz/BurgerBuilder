5ab62440d411ff45de9c5951d425aeba65e5de67
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-09-01 12:22:35 +0200

    Create error hoc with axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,8 +6,9 @@ import AddIngredientContext from '../../context/addIngredient-context';
 import BackDropContext from '../../context/backDrop-context';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import axios from '../../axios-orders';
 
 const INGREDIENTS_PRICING = {
     salad: 0.5,
@@ -116,4 +117,4 @@ class BurgerBuilder extends Component{
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Aux/Aux';
+import BackDropContext from '../../context/backDrop-context';
+const withErrorHandler = (WrapperComponent, axios)=>{
+    return class extends Component {
+
+        state = {error: null};
+
+        componentDidMount() {
+            axios.interceptors.request.use(req =>{
+                this.setState({error: null});
+                return req;
+            })
+            axios.interceptors.response.use(res=>res, error =>{
+                this.setState({error: error});
+            })
+        }
+        errorConfirmedHandler(){
+            this.setState({error: null});
+        }
+
+        render(){
+            return (
+                <Aux>
+                    {
+                      this.state.error?
+                        <BackDropContext.Provider value = {{close: ()=> this.errorConfirmedHandler()}}>
+                            <Modal show={this.state.error}>
+                                {this.state.error? this.state.error.message: null}
+                            </Modal>
+                        </BackDropContext.Provider>:
+                      null
+                    }
+                    <WrapperComponent {...this.props}/>
+                </Aux>
+            )
+        }
+    }
+}
+
+export default withErrorHandler;
