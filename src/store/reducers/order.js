589feb90dfda69ed7e5ed0bd8ecb69976ef43972
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-09-18 20:49:02 +0200

    refactor order reducer

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 const initialState = {
     orders: [],
     loading: false,
@@ -6,51 +7,44 @@ const initialState = {
     errorMsg: null,
     purchased: false
 }
+const purchaseSuccessState = (state, action) => {
+    return {
+        loading: false,
+        purchased: true,
+        orders: state.orders.concat(action.payload)
+    }
+}
 
+const fetchedOrdersState = (action) => {
+    const fetchedOrders = [];
+    for (let key in action.payload.response) {
+        fetchedOrders.push({
+            ...action.payload.response[key],
+            id: key
+        });
+    }
+    return {
+        orders: [...fetchedOrders],
+        loading: false
+    }
+}
+
+const fetchOrderFailureState = (action) => {
+    return {
+        loading: false,
+        error: true,
+        errorMsg: action.payload.response
+    }
+}
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.PURCHASE_BURGER_INIT:
-            return {
-                ...state,
-                purchased: false
-            }
-        case actionTypes.PURCHASE_BURGER_START:
-            return {
-                ...state,
-                loading: true
-            }
-        case actionTypes.PURCHASE_BURGER_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                purchased: true,
-                orders: state.orders.concat(action.payload)
-            }
-        case actionTypes.FETCH_ORDERS:
-            return { ...state, loading: true }
-        case actionTypes.LOAD_ORDERS:
-            const fetchedOrders = [];
-            for (let key in action.payload.response) {
-                fetchedOrders.push({
-                    ...action.payload.response[key],
-                    id: key
-                });
-            }
-            return {
-                ...state,
-                orders: [...fetchedOrders],
-                loading: false
-            }
-        case actionTypes.FETCH_ORDERS_FAILURE:
-            return {
-                ...state,
-                loading: false,
-                error: true,
-                errorMsg: action.payload.response
-            }
-
-        default:
-            return { ...state }
+        case actionTypes.PURCHASE_BURGER_INIT: return updateObject(state, { purchased: false })
+        case actionTypes.PURCHASE_BURGER_START: return updateObject(state, { loading: true })
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return updateObject(state, purchaseSuccessState(state, action))
+        case actionTypes.FETCH_ORDERS: return updateObject(state, { loading: true })
+        case actionTypes.LOAD_ORDERS: return updateObject(state, fetchedOrdersState(action))
+        case actionTypes.FETCH_ORDERS_FAILURE: return updateObject(state, fetchOrderFailureState(action))
+        default: return { ...state }
     }
 }
 
